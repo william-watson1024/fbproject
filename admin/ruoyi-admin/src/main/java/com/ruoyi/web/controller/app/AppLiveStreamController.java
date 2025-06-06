5d006fe7ed555af8521497838f65e1e0596507c6
william-watson1024 <990764375@qq.com>
2025-06-06 23:11:33 +0800

    feat: 添加AppLiveStreamController类，同时完成了各种状态的前后端设置

---
+++ admin/ruoyi-admin/src/main/java/com/ruoyi/web/controller/app/AppLiveStreamController.java
@@ -0,0 +1,13 @@
+package com.ruoyi.web.controller.app;/**
+ * ==================================================
+ *  @Project     : admin
+ *  @Package     : com.ruoyi.web.controller.app
+ *  @Class Name  : AppLiveStreamController
+ *  --------------------------------------------------
+ *  @Author      : William
+ *  @Date        : 2025-06-06 22:13:25
+ *  @Description : TODO
+ * ==================================================
+ */
+public class AppLiveStreamController {
+}
