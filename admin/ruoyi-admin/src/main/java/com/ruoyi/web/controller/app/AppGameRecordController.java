65eb104d3fec5b880c52f6e59c5444a9de42c998
william-watson1024 <990764375@qq.com>
2025-06-06 21:15:30 +0800

    feat: 添加AppGameRecordController类，更新favicon和删除无用图片

---
+++ admin/ruoyi-admin/src/main/java/com/ruoyi/web/controller/app/AppGameRecordController.java
@@ -0,0 +1,13 @@
+package com.ruoyi.web.controller.app;/**
+ * ==================================================
+ *  @Project     : admin
+ *  @Package     : com.ruoyi.web.controller.app
+ *  @Class Name  : AppGameRecordController
+ *  --------------------------------------------------
+ *  @Author      : William
+ *  @Date        : 2025-06-06 20:30:45
+ *  @Description : TODO
+ * ==================================================
+ */
+public class AppGameRecordController {
+}
