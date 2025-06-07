627ab28370970cdb19e2a0547c14073fecca2ee2
william-watson1024 <990764375@qq.com>
2025-06-07 17:20:39 +0800

    fix: 一些bug

---
+++ admin/ruoyi-admin/src/main/java/com/ruoyi/system/Do/SettleGameDTO.java
@@ -0,0 +1,13 @@
+package com.ruoyi.system.Do;/**
+ * ==================================================
+ *  @Project     : admin
+ *  @Package     : com.ruoyi.system.Do
+ *  @Class Name  : SettleGameDTO
+ *  --------------------------------------------------
+ *  @Author      : William
+ *  @Date        : 2025-06-07 17:06:59
+ *  @Description : TODO
+ * ==================================================
+ */
+public class SettleGameDTO {
+}
